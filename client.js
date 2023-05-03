7b0251f98587ef08f50f00099af0310712bd395e
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-05-02 22:41:03 -0600

    cambios

---
+++ client.js
@@ -0,0 +1,26 @@
+const io = require('socket.io-client')
+const serverUrl = "http://192.168.1.132:4000"
+const socket = io(serverUrl)
+
+// Conectar.
+socket.on('connect', () => {
+    console.log("Connected to server")
+
+    socket.emit('signin', {
+        user_name: "Willy",
+        tournament_id: 142857,
+        user_role: 'player'
+    })
+})
+
+// Sign in correcto.
+socket.on('ok_signin', () => {
+    console.log("Login")
+})
+
+// Ready.
+socket.on('ready', function(data){
+    var gameID = data.game_id;
+    var playerTurnID = data.player_turn_id;
+    var board = data.board;
+  });
---
+++ client2.js
@@ -0,0 +1,64 @@
+const io = require('socket.io-client')
+const serverUrl = "http://192.168.1.132:4000"
+const socket = io(serverUrl)
+
+// Conectar.
+socket.on('connect', () => {
+    console.log("Connected to server")
+
+    socket.emit('signin', {
+        user_name: "Willy2",
+        tournament_id: 142857,
+        user_role: 'player'
+    })
+})
+
+// Sign in correcto.
+socket.on('ok_signin', () => {
+    console.log("Login")
+})
+
+// Ready.
+socket.on('ready', function(data){
+    var gameID = data.game_id;
+    var playerTurnID = data.player_turn_id;
+    var board = data.board;
+  });
+
+  // Finish.
+socket.on('finish', function(data){
+    var gameID = data.game_id;
+    var playerTurnID = data.player_turn_id;
+    var winnerTurnID = data.winner_turn_id;
+    var board = data.board;
+});
+
+socket.on('ready', function(data){
+    var gameID = data.game_id;
+    var playerTurnID = data.player_turn_id;
+    var board = data.board;
+    
+    // TODO: Your logic / user input here
+    
+    socket.emit('play', {
+      tournament_id: tournamentID,
+      player_turn_id: playerTurnID,
+      game_id: gameID,
+      movement: 5
+    });
+  });
+
+socket.on('finish', function(data){
+  var gameID = data.game_id;
+  var playerTurnID = data.player_turn_id;
+  var winnerTurnID = data.winner_turn_id;
+  var board = data.board;
+  
+  // TODO: Your cleaning board logic here
+  
+  socket.emit('player_ready', {
+    tournament_id: tournamentID,
+    player_turn_id: playerTurnID,
+    game_id: gameID
+  });
+});
